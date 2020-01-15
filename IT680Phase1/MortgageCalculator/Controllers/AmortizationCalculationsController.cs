43eb64f709eb6ad1ba0ccfd6fad2406cea75dbd1
Ayodele <ayodele.olufemi@mnsu.edu>
2020-01-15 00:43:29 -0600

    Succesfully coded amortization table

---
+++ IT680Phase1/MortgageCalculator/Controllers/AmortizationCalculationsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MortgageCalculator.Models;
+
+namespace MortgageCalculator.Controllers
+{
+    
+    [ApiController]
+    public class AmortizationCalculationsController : ControllerBase
+    {
+        [Route("PrintAmortization")]
+        [HttpPost]
+        public ActionResult<List<AmortizationDataOut>> GetAmortizationTable(MortgageDataOut dataOut)
+        {
+            List<AmortizationDataOut> result = new List<AmortizationDataOut>();
+            var numberOfMonths = (dataOut.InputedData.DurationYears * 12) + dataOut.InputedData.DurationMonths;
+            double loanAmount = dataOut.InputedData.LoanAmount;
+            for (int i = 1; i <= numberOfMonths; i++)
+            {
+                double monthlyInterest = loanAmount * dataOut.InputedData.AnnualRate / 1200;
+                var thisPeriod = new AmortizationDataOut(i, dataOut.MonthlyPayment, monthlyInterest, dataOut.MonthlyPayment - monthlyInterest, loanAmount - dataOut.MonthlyPayment + monthlyInterest);
+                loanAmount = loanAmount - dataOut.MonthlyPayment + monthlyInterest;
+                result.Add(thisPeriod);
+            }
+            return result;
+        }
+    }
+}
---
+++ IT680Phase1/MortgageCalculator/Models/AmortizationDataOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator.Models
+{
+    public class AmortizationDataOut
+    {
+        public AmortizationDataOut(int months, double amount, double interest, double principal, double balance)
+        {
+            Months = months;
+            Amount = amount;
+            Interest = interest;
+            Principal = principal;
+            Balance = balance;
+        }
+        public int Months { get; set; }
+        public double Amount { get; set; }
+        public double Interest { get; set; }
+        public double Principal { get; set; }
+        public double Balance { get; set; }
+    }
+}
