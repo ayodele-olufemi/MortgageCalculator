93f98d0a00afebfc8ae4762efcafb64d13b17cee
Ayodele <ayodele.olufemi@mnsu.edu>
2020-01-15 23:07:40 -0600

    Trying to start model binding for WPFApp

---
+++ IT680Phase1/MortgageCalculatorWebApp/Models/MortgageDataIn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortgageCalculatorWebApp.Models
+{
+    public class MortgageDataIn
+    {
+        public double LoanAmount { get; set; }
+        public double AnnualRate { get; set; }
+        public int DurationYears { get; set; }
+        public int DurationMonths { get; set; }
+        public Customer CustomerDetails { get; set; }
+    }
+}
